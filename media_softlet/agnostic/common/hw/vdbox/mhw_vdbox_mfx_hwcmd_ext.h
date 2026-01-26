bff6796f92a74a272d1a99b7cc2c1a8dbf6d507d
Cheah, Vincent Beng Keat <vincent.beng.keat.cheah@intel.com>
2026-01-26 15:50:05 +0800

    Fix MHW interface for AVC 10 decode

---
+++ media_softlet/agnostic/common/hw/vdbox/mhw_vdbox_mfx_hwcmd_ext.h
@@ -0,0 +1,35 @@
+/*===================== begin_copyright_notice ==================================
+
+INTEL CONFIDENTIAL
+Copyright 2019-2022
+Intel Corporation All Rights Reserved.
+
+The source code contained or described herein and all documents related to the
+source code ("Material") are owned by Intel Corporation or its suppliers or
+licensors. Title to the Material remains with Intel Corporation or its suppliers
+and licensors. The Material contains trade secrets and proprietary and confidential
+information of Intel or its suppliers and licensors. The Material is protected by
+worldwide copyright and trade secret laws and treaty provisions. No part of the
+Material may be used, copied, reproduced, modified, published, uploaded, posted,
+transmitted, distributed, or disclosed in any way without Intel's prior express
+written permission.
+
+No license under any patent, copyright, trade secret or other intellectual
+property right is granted to or conferred upon you by disclosure or delivery
+of the Materials, either expressly, by implication, inducement, estoppel
+or otherwise. Any license under such intellectual property rights must be
+express and approved by Intel in writing.
+
+======================= end_copyright_notice ==================================*/
+//!
+//! \file     mhw_vdbox_mfx_hwcmd_ext.h
+//! \brief    constructors for MHW and states.
+//! \details  This file may not be included outside as other components
+//!           should use MHW interface to interact with MHW commands and states.
+//!
+#ifndef __MHW_VDBOX_MFX_HWCMD_EXT_H__
+#define __MHW_VDBOX_MFX_HWCMD_EXT_H__
+
+#define MFX_MPEG2_PIC_STATE_CMD_DW3_BIT24_28 OldbpquantEncoderThisFieldShouldBeSetToZero
+
+#endif  // __MHW_VDBOX_MFX_HWCMD_EXT_H__
